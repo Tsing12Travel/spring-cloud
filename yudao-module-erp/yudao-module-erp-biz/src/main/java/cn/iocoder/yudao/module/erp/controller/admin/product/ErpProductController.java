fac65b89e30786b5113aafe8f5880a5f3141c628
Tsing12Travel <Tsing12Travel@163.com>
2025-02-16 17:58:00 +0800

    renew

--- yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/controller/admin/product/ErpProductController.java
+++
@@ -1,9 +0,0 @@
-package cn.iocoder.yudao.module.erp.controller.admin.product;
-
-import cn.iocoder.yudao.module.erp.service.product.ErpProductService;
-import jakarta.annotation.Resource;
-
-public class ErpProductController {
-    @Resource
-    private ErpProductService productService;
-}
