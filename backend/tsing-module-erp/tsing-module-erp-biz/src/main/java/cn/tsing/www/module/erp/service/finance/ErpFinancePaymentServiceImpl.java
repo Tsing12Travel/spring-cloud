b4044debebb0ebc36c37d4025cec536575a81e84
Tsing12Travel <Tsing12Travel@163.com>
2025-03-17 17:00:21 +0800

    ErpFinancePaymentServiceImpl

--- backend/tsing-module-erp/tsing-module-erp-biz/src/main/java/cn/tsing/www/module/erp/service/finance/ErpFinancePaymentServiceImpl.java
+++ backend/tsing-module-erp/tsing-module-erp-biz/src/main/java/cn/tsing/www/module/erp/service/finance/ErpFinancePaymentServiceImpl.java
@@ -35,12 +35,10 @@ import static cn.tsing.www.framework.common.exception.util.ServiceExceptionUtil.
 import static cn.tsing.www.framework.common.util.collection.CollectionUtils.*;
 import static cn.tsing.www.module.erp.enums.ErrorCodeConstants.*;
 
-// TODO 芋艿：记录操作日志
+// TODO 记录操作日志
 
 /**
  * ERP 付款单 Service 实现类
- *
- * @author 芋道源码
  */
 @Service
 @Validated
